95487aebd9cbe1fc810bff7b862f3d920508c428
mayuri-patil123 <mayuripatil200725@gmail.com>
2026-09-16 10:11:25 +0530

    javascript setup

---
+++ variables.js
@@ -0,0 +1,16 @@
+const accountId =144553
+let accountEmail = "hitesh@google.com"
+// var accountPassword ="12345"  these (var)variable is not used mainly at the palce of it we us (let )mostly.
+accoountCity = "jaipur"
+let accountState
+
+// accountId = 2  these type of change is not allowed
+
+// here below i have wriiten same variables
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accoountCity = "Bengualuru"
+
+console.log(accountId);
+
+console.table ([accountId,accountEmail,accountPassword,accoountCity])
