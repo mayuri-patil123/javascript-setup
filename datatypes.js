b429d204887fda4f3ebed668c68dac42ab68ecf6
mayuri-patil123 <mayuripatil200725@gmail.com>
2026-09-17 18:37:08 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict";   //treat all js code as newer versio
+
+// alert(3+3) //we are using noede.js ,not browaer
+
+console.log("mayuri")
+
+let name = "mayuri"
+let age = 19
+let isLoggedIn = "false"
+let state = "null"
+
+// number => 2 to the power 53
+// bigint = when big amount of numbe is used
+// string =>""
+// boolean = true/false
+// null => standalone value
+// undefined 
+// symbol =>unique
+
+
+// object =>used the most
+console.log(typeof age);
