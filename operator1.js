929285b438e40d3c750ba5810e6e10f9314f2a61
mayuri-patil123 <mayuripatil200725@gmail.com>
2026-09-22 23:45:34 +0530

    javascript-setup

---
+++ operator1.js
@@ -0,0 +1,24 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 ="mayuri"
+let str2 ="hello"
+
+let str3 = str1+str2
+console.log(str3);
+ 
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
