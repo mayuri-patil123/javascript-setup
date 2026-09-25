f642038501eb1b280b819e506b6a6334e1b8d580
mayuri-patil123 <mayuripatil200725@gmail.com>
2026-09-25 11:45:23 +0530

    date-time.js

---
+++ date-time.js
@@ -0,0 +1,25 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23,5,3)
+let myCreatedDate = new Date()
+
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+let myTimestamp = Date.now()
+console.log(myTimestamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+
+newDate.toLocaleString('default', {
+    weekday:"long",
+})
